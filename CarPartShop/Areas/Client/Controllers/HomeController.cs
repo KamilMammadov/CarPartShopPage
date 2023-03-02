b57c4003a156872ea0a7d96909a70f1b927df94b
KamilMammadov <kamiltm@code.edu.az>
2023-03-02 15:45:09 +0400

    layout created. wwwroot configured

---
+++ CarPartShop/Areas/Client/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarPartShop.Areas.Client.Controllers
+{
+    [Area("client")]
+    public class HomeController : Controller
+    {
+       
+        [HttpGet("/", Name = "client-home-index")]
+        [HttpGet("index")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      
+    }
+}
