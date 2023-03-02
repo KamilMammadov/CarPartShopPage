b75938c1d9dbccc8fb28486a8283c0f64cb8d09f
KamilMammadov <kamiltm@code.edu.az>
2023-03-02 14:39:25 +0400

    Initual commit

---
+++ CarPartShop/Database/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarPartShop.Database
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options)
+        : base(options)
+        {
+
+        }
+    }
+}
