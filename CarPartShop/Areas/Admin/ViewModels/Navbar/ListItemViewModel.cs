a00421d3a555902623a9e29f5379e8bd92c096d8
KamilMammadov <kamiltm@code.edu.az>
2023-03-28 14:38:14 +0400

    Navbar admin added

---
+++ CarPartShop/Areas/Admin/ViewModels/Navbar/ListItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace CarPartShop.Areas.Admin.ViewModels.Navbar
+{
+    public class ListItemViewModel
+    {
+        public ListItemViewModel(int id,string title, int order, string url)
+        {
+            Id = id;
+            Title = title;
+            Order = order;
+            Url = url;
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Order { get; set; }
+        public string Url { get; set; }
+        
+    }
+}
---
+++ CarPartShop/Areas/Admin/ViewModels/Navbar/UpdateViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CarPartShop.Areas.Admin.ViewModels.Navbar
+{
+    public class UpdateViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public int Order { get; set; }
+    }
+}
